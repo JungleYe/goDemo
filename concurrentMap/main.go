ba5333f6165d1584cad32727fbfa05e739049008
JunlgeYe <395321108@qq.com>
2019-05-10 14:25:58 +0800

    并发map测试

---
+++ concurrentMap/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"sync"
+	"runtime"
+	"log"
+	"time"
+)
+
+type st struct{
+	A int32
+	A1 int32
+	A2 int32
+	A3 int32
+	A4 int32
+	A5 int32
+	A6 int32
+	A7 int32
+	A8 int32
+	A9 int32
+	A10 int32
+	A11 int32
+	A12 int32
+	B int32
+	Mutex sync.Mutex
+}
+
+func main(){
+	runtime.GOMAXPROCS(6)
+	var mp map[string]st
+	mp = make(map[string]st)
+	mp["k"] = st{A:-1,B:2,Mutex:sync.Mutex{}}
+
+	//并发的修改map
+	for i:= 1;i<=100000;i++{
+		go func(ii int){
+			v := mp["k"]
+			v.A = int32(ii)
+			v.A1 = int32(ii)
+			v.A2 = int32(ii)
+			v.A3 = int32(ii)
+			v.A4 = int32(ii)
+			v.A5 = int32(ii)
+			v.A6 = int32(ii)
+			v.A7 = int32(ii)
+			v.A8 = int32(ii)
+			v.A9 = int32(ii)
+			v.A10 = int32(ii)
+			v.Mutex.Lock()
+			mp["k"] = v
+			v.Mutex.Unlock()
+		}(i)
+	}
+
+	for j:=0;j<=100000;j++{
+		go func(jj int){
+			v := mp["k"]
+			if v.A ==0{
+				log.Println("illegal happened!")
+			}
+		}(j)
+	}
+
+	time.Sleep(500 * time.Second)
+}
