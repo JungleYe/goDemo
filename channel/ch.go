e25ee222ad6570bca43d7f7b175b1b57c0c0d509
JunlgeYe <395321108@qq.com>
2019-06-01 16:22:49 +0800

    危险：初始化的channel小心堵死

---
+++ channel/ch.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+type st struct{
+	name string
+	age int32
+}
+var ch  chan *st
+func main(){
+
+	//知识点：没有经过make的channel，就等着被锁死吧；windows下会有这个提示：fatal error: all goroutines are asleep - deadlock!
+	ch = make(chan *st)
+	go Dispatch()
+
+	time.Sleep( time.Second)
+	var oneSt = &st{"jungle",29}
+	ch <- oneSt
+	time.Sleep(2 * time.Second)
+	fmt.Println("the end")
+}
+
+func  Dispatch() error {
+	fmt.Println("开启了Dispatch")
+	for {
+		select {
+		case req := <- ch:
+			fmt.Println("可以从channel中拿到数据了！")
+			fmt.Println(req)
+		}
+	}
+	return nil
+}
